5d9d5a30e14d504e18f542a91bcf6d433504f408
yujin <minishelll777@gmail.com>
2022-04-19 12:51:41 +0900

    capture

--- cam/models.py
+++ cam/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+	image_name = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.image_name
