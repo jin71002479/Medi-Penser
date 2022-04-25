e600252c01f1e9d13cc39cb02970e597ea7b7ac6
yujin <minishelll777@gmail.com>
2022-04-25 19:02:05 +0900

    test

--- cam/urls.py
+++ cam/urls.py
@@ -1,14 +1,18 @@
 
 from django.conf import settings
 from django.urls import path
-from cam.views import camera_1,video
+from cam.views import camera_1,video,md,cap
 from django.conf.urls.static import static
+
 
 app_name='cam'
 
 urlpatterns = [
     path('video_1/', video, name="video"),
     path('show/',camera_1,name='show'),
+    path('cap/',cap,name='cap'),
+    # path('train/',getImagesAndLabels),
+    path('mod/',md,name='mod'),
 ]+ static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
