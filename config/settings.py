667655fa06a826f79170369f9aaa25952be0d522
YoonYoon <jin71002479@gmail.com>
2022-04-29 00:58:06 +0900

    20220429-12:58

--- config/settings.py
+++ config/settings.py
@@ -127,9 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
