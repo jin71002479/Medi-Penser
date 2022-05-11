7f921a575635ff649c332370f973d048f6cf0d4b
yujin <minishelll777@gmail.com>
2022-05-11 11:15:07 +0900

    chat

---
+++ train.py
@@ -0,0 +1,69 @@
+import json
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from tensorflow import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+
+with open('model/intents.json','r', encoding="UTF-8") as file:
+    data = json.load(file)
+    
+training_sentences = []
+training_labels = []
+labels = []
+responses = []
+
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        training_sentences.append(pattern)
+        training_labels.append(intent['tag'])
+        responses.append(intent['responses'])
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+num_classes = len(labels)
+
+lbl_encoder = LabelEncoder()
+lbl_encoder.fit(training_labels)
+training_labels = lbl_encoder.transform(training_labels)
+
+vocab_size = 1000
+embedding_dim = 16
+max_len = 50
+oov_token = "<OOV>"
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
+
+# Model Training
+keras.backend.clear_session()
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(32, activation='selu'))
+model.add(Dense(32, activation='selu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+
+epochs = 700
+history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
+
+# to save the trained model
+model.save("model/chat_model.h5")
+
+import pickle
+
+# to save the fitted tokenizer
+with open('model/tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# to save the fitted label encoder
+with open('model/label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
